04f9ef03bf7fd3151903b7c9ccfa58413df8742e
juriserts <93523283+juriserts@users.noreply.github.com>
2021-11-24 13:36:59 +0200

    Update pamatfails.py

--- pamatfails.py
+++ pamatfails.py
@@ -7,6 +7,3 @@ Pārbaudiet vai programma strādā pareizi, saglabājiet, izveidojiet COMMIT, no
 """
 
 print("Mans vārds ir Juris")
-print("Mans vārds ir laura")
-print("Mans vārds ir Mārtīņš!")
-print("Mans vārds ir Emīls")
